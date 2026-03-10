90548785683fb62cec2beebb99ab6c5b26713fbd
Varivonchik Artem <varivonchik.core@gmail.com>
2026-03-11 02:24:18 +0300

    feat(string): implementation of the roman to integer algorithm

--- String/String.py
+++ String/String.py
@@ -77,3 +77,30 @@ class String:
                 return False
 
         return i == m and j == n
+
+    def romanToInt(self, s: str) -> int:
+
+        roman_symbols: dict[str, int] = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+
+        total = 0
+        prev = 0
+
+        for char in reversed(s):
+            curr = roman_symbols[char]
+
+            if curr < prev:
+                total -= curr
+            else:
+                total += curr
+
+            prev = curr
+
+        return total
